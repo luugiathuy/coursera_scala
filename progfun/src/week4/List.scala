1903a7ec6f34dbea2fc36ac7ae9690ceebdaca0e
Luu Gia Thuy <luugiathuy@gmail.com>
2014-10-11 12:42:29 +0800

    variance

--- progfun/src/week4/List.scala
+++ progfun/src/week4/List.scala
@@ -1,9 +1,29 @@
 package week4
 
-import week3._;
+trait List[+T] {
+	def isEmpty: Boolean
+	def head: T
+	def tail: List[T]
+	def prepend[U >: T](elem: U): List[U] = new Cons(elem, this)
+}
+
+class Cons[T](val head: T, val tail: List[T]) extends List[T] {
+  def isEmpty = false
+}
+
+object Nil extends List[Nothing] {
+  def isEmpty: Boolean = true
+  def head: Nothing = throw new NoSuchElementException("Nil.head")
+  def tail: Nothing = throw new NoSuchElementException("Nil.tail")
+}
 
 object List {
-	def apply[T](x: T): List[T] = new Cons(x, new Nil)
-	def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
-	def apply[T]() = new Nil
+	def apply[T](x: T): List[T] = new Cons(x, Nil)
+	def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, Nil))
+	def apply[T]() = Nil
+}
+
+object test {
+  val x: List[String] = Nil
+  def f(xs: List[NonEmpty], x: Empty) = xs prepend x
 }
